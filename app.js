887d0555505f675629c70fe5c20abd75d2c35970
Ashyzan <info@studiocreativo69.it>
2024-03-14 13:25:32 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+console.log('funziona');
+
+
+// // verifichiamo se un numero è pari o dispari
+// const numero = 11;
+// let resto = numero % 2;
+// console.log('resto: ', resto);
+// const divisione = numero / 2;
+// console.log('divisione:', divisione);
+// // mi calcolo il modulo 2
+// //SE il resto è 0
+// // if (resto === 0){
+// //     console.log('il numero è pari')
+// // } else{
+// //     console.log('il numero è dispari')
+// // }
+// if(resto !== 0){
+//     console.log('il numero è dispari')
+// } else{
+//     console.log('il numero è pari')
+// }
+// // - scrivo il numero è pari
+// //ALTRIMENTI
+// // - scrivo il numero è dispari
+
+
+// // verifichiamo se una persona può votare
+// //età per votare 18 anni;
+// let eta = prompt('inserisci la tua età');
+// eta = parseInt(eta);
+// console.log(eta);
+// //SE età >= a 18
+// if (eta >= 18) {
+//     console.log('puoi votare')
+// } else {
+//     console.log('non puoi votare')
+// }
+
+
+
+
+//BIGLIETTO DLE TRENO CON PROMPT
+
+// chiedo i kilometri da percorrere
+let kilometriElement = prompt('inserisci i km da percorrere');
+kilometriElement = parseInt(kilometriElement);
+console.log(kilometriElement);
+
+// chiedo l'età
+let etaElement = prompt('inserisci la tua età');
+etaElement = parseInt(etaElement);
+console.log(etaElement);
